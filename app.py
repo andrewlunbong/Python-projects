a02c8ef609657f2b1eac058426a009d86a8f9124
andrewlunbong <andrewlunbong@gmail.com>
2024-05-08 23:48:23 +0100

    working progress

---
+++ app.py
@@ -0,0 +1,86 @@
+# first = input("First: ")
+# second = input("Second: ")
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
+# course = "Python for Beginners"
+# print("Python" in course)
+# find the word python in the object 
+
+# print(10 / 3) get the float number
+# print(10 // 3) get the the whole number
+# print(10 % 3) get the the remainder of 10 devided by 3 which is = 1
+# print(10 ** 3) the answer is 10 to the power of 3 = 1000
+
+
+# x = 10
+# x = x + 3
+# x += 3 this line is the same as the above
+
+# x = (10 + 3) * 2 if you don't put in the parenthesis then 3 x 2 would be first and the answer would be 16
+# print(x)
+
+# x = 3 != 2
+# print(x) this is asking if 3 is not equal to
+
+# price = 25
+# print(price > 10 and price < 30) if both of the expression is right then it'll turn true
+
+# price = 5
+# print(price > 10 or price < 30) if one of the expression is right then it'll turn true
+
+
+# price = 5
+# print(not price > 10) In Python, the "not" logical operator is used to negate the truth value of a boolean expression. It returns True if the expression is False, and False if the expression is True.
+
+# temperature = 25
+
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif temperature > 20: #(20, 30)
+#     print("it's a nice day")
+# elif temperature > 10:
+#     print("It's a bit cold")
+# else:
+#     print("It's cold")
+# print("Done")
+
+# weight = int(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in Lbs: " + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("Weight in Kgs: " + str(converted))
+
+# i = 1
+# while i <= 10:
+#     print(i * "*")
+#     i = i + 1 // while loop 
+
+# names = ["John", "Andrew", "Shristi", "Bob", "Josh"]
+# names[0] = "Jon"
+# print(names[0:3])
+# print(names) // finding the list using index number
+
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers)) // find how many element in the numbers
+
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print(item) using for loop to check how many item is in the numbers
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i + 1 // answer is the same as for loop and this way is more than the above one
+
+# numbers = range(5, 10, 2)
+# for number in range(5):
+#     print(number) // the top line is not important as the answer of the range is 0,1,2,3,4
+
+numbers = (1, 2, 3, 3)
+numbers.count
+
